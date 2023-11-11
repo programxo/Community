2fe5ef45696dde8428966885fcf055a4a35c9950
Abdul Halim-Ukaiev-FHV <halimukajev@gmail.com>
2023-11-11 16:10:57 +0100

    Update: routes, base.html

--- web/routes.py
+++ web/routes.py
@@ -66,5 +66,5 @@ def new_idea():
         idea = Idea(content=form.content.data, user_id=current_user.id)
         db.session.add(idea)
         db.session.commit()
-        return redirect(url_for('web.idea'))  # Oder eine andere Zielseite
-    return render_template('idea.html', title='Neue Idee', form=form)
+        return redirect(url_for('web.home'))  # Oder eine andere Zielseite
+    return render_template('idea.html', title='New Idea', form=form)
